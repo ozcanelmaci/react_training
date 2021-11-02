b4c7a571fc352bfe234e669cff501f576a5b28db
Özcan Elmacı <ozcan.elmaci@ogr.deu.edu.tr>
2021-11-03 01:15:42 +0300

    Preparing API infrastructure

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class ProductList extends Component {
-    render() {
-        return (
-            <div>
-                <h3>Product List</h3>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h3>{this.props.info.title}-{this.props.currentCategory}</h3>
+      </div>
+    );
+  }
 }
